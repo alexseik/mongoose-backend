4063313deeaf0434665f4258fa90e223af691fc4
agarridolopez <Alejandro.Garrido@eurotaxglass.com>
2015-09-28 23:56:41 +0200

    [US] Add simple failing test with mocha

--- test/e2e/app.js
+++ test/e2e/app.js
@@ -1,1 +1,13 @@
+/*global require,describe,it*/
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
 
+describe('Rest API', function (){
+    'use strict';
+    describe ('Products',function () {
+        it('should error trying to save product without ean13', function (){
+            (true).should.be.exactly(false);
+        });
+    });
+});
